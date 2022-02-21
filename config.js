62244e3952c273ade5f546040a4fa0b1ea684580
h0x1 <37070924+h0x1@users.noreply.github.com>
2022-02-21 21:46:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     dsc: {
-        token: ''
+        token: 'OTQ1Mzc3NTk4NDMzNzg3OTA0.YhPRfw.MIV0gPz4x4U6SX63OanoJzmmhqE'
     },
 
     app: {
-        prefix: '!'
+        prefix: '/'
     }
 };
